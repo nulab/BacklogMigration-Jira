2fb2ac8a893c3d9b146cd3d3e79577d7f983ced0
Shoma Nishitateno <shoma.nishitateno@nulab-inc.com>
2017-10-13 15:42:32 +0900

    Make to fetch users recursive

--- jira-client/src/main/scala/com/nulabinc/jira/client/apis/impl/UserRestClientImpl.scala
+++ jira-client/src/main/scala/com/nulabinc/jira/client/apis/impl/UserRestClientImpl.scala
@@ -11,16 +11,7 @@ class UserRestClientImpl(httpClient: HttpClient) extends UserRestClient {
 
   import UserMappingJsonProtocol._
 
-  def users: Either[JiraRestClientError, Seq[User]] = {
-    val encodedUrl = URLEncoder.encode("%", "UTF-8")
-    val url = "/user/search?startAt=0&maxResults=1000&username=" + encodedUrl
-    val body = httpClient.get(url)
-    body match {
-      case Right(users) => Right(JsonParser(users).convertTo[Seq[User]])
-      case Left(error)  => Left(HttpError(error.toString))
-    }
-
-  }
+  def users: Either[JiraRestClientError, Seq[User]] = chunkUsers(Seq.empty[User])
 
   def user(name: String): Either[JiraRestClientError, User] = {
     httpClient.get(s"/user?key=$name") match {
@@ -28,5 +19,20 @@ class UserRestClientImpl(httpClient: HttpClient) extends UserRestClient {
       case Left(_: ApiNotFoundError) => Left(ResourceNotFoundError("user", name))
       case Left(error)               => Left(HttpError(error.toString))
     }
+  }
+
+  private [this] def chunkUsers(array: Seq[User], startAt: Int = 0): Either[JiraRestClientError, Seq[User]] = {
+    val maxResults = 100
+    val encodedUrl = URLEncoder.encode("%", "UTF-8")
+    val url = s"/user/search?startAt=$startAt&maxResults=$maxResults&username=" + encodedUrl
+    val body = httpClient.get(url)
+    val result = body match {
+      case Right(json) => Right(JsonParser(json).convertTo[Seq[User]])
+      case Left(error)  => Left(HttpError(error.toString))
+    }
+
+    if (result.isLeft) result
+    else if (result.right.get.isEmpty) Right(array)
+    else chunkUsers(array ++ result.right.get, startAt + maxResults)
   }
 }
