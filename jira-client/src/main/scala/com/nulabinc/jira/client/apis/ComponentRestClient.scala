52dc301f30dbddf4e53fa191988949ae5be46e18
Shoma Nishitateno <shoma.nishitateno@nulab-inc.com>
2017-10-25 11:54:25 +0900

    Component client no imply

---
+++ jira-client/src/main/scala/com/nulabinc/jira/client/apis/ComponentRestClient.scala
@@ -0,0 +1,12 @@
+package com.nulabinc.jira.client.apis
+
+import com.nulabinc.jira.client.JiraRestClientError
+import com.nulabinc.jira.client.domain.Component
+
+trait ComponentRestClient {
+
+  def projectComponents(id: Long): Either[JiraRestClientError, Seq[Component]]
+
+  def projectComponents(projectKey: String): Either[JiraRestClientError, Seq[Component]]
+
+}
---
+++ jira-client/src/main/scala/com/nulabinc/jira/client/apis/impl/ComponentRestClientImpl.scala
@@ -0,0 +1,10 @@
+package com.nulabinc.jira.client.apis.impl
+
+import com.nulabinc.jira.client.apis.ComponentRestClient
+
+class ComponentRestClientImpl extends ComponentRestClient {
+
+  override def projectComponents(id: Long) = ???
+
+  override def projectComponents(projectKey: String) = ???
+}
