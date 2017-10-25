44e1e23d249c8f72ad8777b48a0b1ddc46f93adb
Shoma Nishitateno <shoma.nishitateno@nulab-inc.com>
2017-10-25 11:53:46 +0900

    Refactor

--- jira-client/src/main/scala/com/nulabinc/jira/client/domain/Field.scala
+++ jira-client/src/main/scala/com/nulabinc/jira/client/domain/Field.scala
@@ -19,17 +19,17 @@ object FieldSchemaType {
       case "number"             => NumberSchema
       case "string"             => StringSchema
       case "date"               => DateSchema
+      case "array"              => ArraySchema
+      case "datetime"           => DatetimeSchema
+      case "user"               => UserSchema
       case "any"                => AnySchema
       case "option"             => OptionSchema
       case "option-with-child"  => OptionWithChildSchema
       case "issuetype"          => IssueTypeSchema
       case "project"            => ProjectSchema
-      case "array"              => ArraySchema
       case "resolution"         => ResolutionSchema
-      case "datetime"           => DatetimeSchema
       case "watches"            => WatchesSchema
       case "priority"           => PrioritySchema
-      case "user"               => UserSchema
       case "status"             => StatusSchema
       case "timetracking"       => TimeTrackingSchema
       case "securitylevel"      => SecurityLevelSchema
@@ -43,18 +43,18 @@ object FieldSchemaType {
 case object NumberSchema extends FieldSchemaType
 case object StringSchema extends FieldSchemaType
 case object DateSchema extends FieldSchemaType
+case object ArraySchema extends FieldSchemaType
+case object DatetimeSchema extends FieldSchemaType
+case object UserSchema extends FieldSchemaType
 case object AnySchema extends FieldSchemaType
 case object OptionSchema extends FieldSchemaType
 case object OptionWithChildSchema extends FieldSchemaType
 
 case object IssueTypeSchema extends FieldSchemaType
 case object ProjectSchema extends FieldSchemaType
-case object ArraySchema extends FieldSchemaType
 case object ResolutionSchema extends FieldSchemaType
-case object DatetimeSchema extends FieldSchemaType
 case object WatchesSchema  extends FieldSchemaType
 case object PrioritySchema extends FieldSchemaType
-case object UserSchema extends FieldSchemaType
 case object StatusSchema extends FieldSchemaType
 case object TimeTrackingSchema extends FieldSchemaType
 case object SecurityLevelSchema extends FieldSchemaType
