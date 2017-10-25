d47ad9539acfd85c2e0718ce67ee040f7338f0ff
Shoma Nishitateno <shoma.nishitateno@nulab-inc.com>
2017-10-25 11:54:40 +0900

    Add writes pending

---
+++ project-writer/src/main/scala/com/nulabinc/backlog/j2b/issue/writer/convert/CustomFieldWrites.scala
@@ -0,0 +1,43 @@
+package com.nulabinc.backlog.j2b.issue.writer.convert
+
+import javax.inject.Inject
+
+import com.nulabinc.backlog.migration.common.convert.Writes
+import com.nulabinc.backlog.migration.common.domain.BacklogCustomField
+import com.nulabinc.backlog.migration.common.utils.Logging
+import com.nulabinc.backlog4j.CustomField.FieldType
+import com.nulabinc.jira.client.domain._
+
+private [writer] class CustomFieldWrites @Inject()(customFieldDefinition: Seq[Field])
+    extends Writes[IssueField, Option[BacklogCustomField]]
+    with Logging {
+
+  override def writes(issueField: IssueField) = {
+    customFieldDefinition.find(_.id == issueField.id) match {
+      case Some(field) =>
+        field.schema.map { schema =>
+          schema.schemaType match {
+            case StringSchema => toTextCustomField(field.name, issueField.value.asInstanceOf[StringFieldValue])
+            case NumberSchema => toNumberCustomField(field.name, issueField.value.asInstanceOf[NumberFieldValue])
+
+          }
+        }
+    }
+  }
+
+  private [this] def toTextCustomField(name: String, issueField: StringFieldValue) =
+    BacklogCustomField(
+      name = name,
+      fieldTypeId = FieldType.Text.getIntValue,
+      optValue = Some(issueField.value),
+      values = Seq.empty[String]
+    )
+
+  private [this] def toNumberCustomField(name: String, issueField: NumberFieldValue) =
+    BacklogCustomField(
+      name = name,
+      fieldTypeId = FieldType.Numeric.getIntValue,
+      optValue = Some(issueField.value.toString),
+      values = Seq.empty[String]
+    )
+}
