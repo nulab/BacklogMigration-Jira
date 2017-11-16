db6e2e83428fc7beb3de652bed9842fe8bf9b068
Shoma Nishitateno <shoma.nishitateno@nulab-inc.com>
2017-11-16 09:42:50 +0900

    Move writer

--- exporter/src/main/scala/com/nulabinc/backlog/j2b/exporter/IssueFileWriter.scala
+++ exporter/src/main/scala/com/nulabinc/backlog/j2b/exporter/IssueFileWriter.scala
@@ -1,21 +1,34 @@
-package com.nulabinc.backlog.j2b.issue.writer
+package com.nulabinc.backlog.j2b.exporter
 
 import java.util.Date
 import javax.inject.Inject
 
 import com.nulabinc.backlog.j2b.issue.writer.convert.IssueWrites
+import com.nulabinc.backlog.j2b.jira.service.IssueService
 import com.nulabinc.backlog.j2b.jira.writer.IssueWriter
-import com.nulabinc.backlog.migration.common.domain.BacklogJsonProtocol._
 import com.nulabinc.backlog.migration.common.conf.BacklogPaths
 import com.nulabinc.backlog.migration.common.domain.BacklogIssue
+import com.nulabinc.backlog.migration.common.domain.BacklogJsonProtocol._
 import com.nulabinc.backlog.migration.common.utils.IOUtil
 import spray.json._
 
 class IssueFileWriter @Inject()(implicit val issueWrites: IssueWrites,
-                               backlogPaths: BacklogPaths) extends IssueWriter {
+                                backlogPaths: BacklogPaths,
+                                issueService: IssueService) extends IssueWriter {
 
   override def write(backlogIssue: BacklogIssue, issueCreatedAt: Date) = {
     val issueDirPath = backlogPaths.issueDirectoryPath("issue", backlogIssue.id, issueCreatedAt, 0)
+
+    // download
+    val dir  = backlogPaths.issueAttachmentDirectoryPath(issueDirPath)
+    backlogIssue.attachments.foreach { attachment =>
+      IOUtil.createDirectory(dir)
+      val path = backlogPaths.issueAttachmentPath(dir, attachment.name)
+      attachment.optId.map { id =>
+        issueService.downloadAttachments(id, path, attachment.name)
+      }
+    }
+
     IOUtil.output(backlogPaths.issueJson(issueDirPath), backlogIssue.toJson.prettyPrint)
     Right(backlogIssue)
   }
