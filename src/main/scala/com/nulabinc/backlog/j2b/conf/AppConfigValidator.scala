adf3a2dd4bd0d909d859f1ba938d0fe10b1f21cf
Shoma Nishitateno <shoma.nishitateno@nulab-inc.com>
2017-11-16 11:50:29 +0900

    Refactor validation

--- src/main/scala/com/nulabinc/backlog/j2b/conf/AppConfigValidator.scala
+++ src/main/scala/com/nulabinc/backlog/j2b/conf/AppConfigValidator.scala
@@ -6,59 +6,50 @@ import com.nulabinc.backlog.migration.common.utils.{ConsoleOut, Logging}
 import com.nulabinc.jira.client._
 import com.osinka.i18n.Messages
 
-class AppConfigValidator @Inject()(jiraRestClient: JiraRestClient) extends Logging {
+sealed trait ConfigValidateResult
+case object ConfigValidateSuccess extends ConfigValidateResult
+case class ConfigValidateFailure(reason: String) extends ConfigValidateResult
 
-  def validate(config: AppConfiguration): List[ConfigValidateResult] = {
-    List(
-      validateProjectKey(config.backlogProjectKey),
-      validateConfigJira()
-    ).filter {
-      case Success()  => false
-      case Failure(_) => true
-    }
-  }
+
+object AppConfigValidator extends Logging {
 
   def validateProjectKey(projectKey: String): ConfigValidateResult =
-    if (projectKey.matches("""^[0-9A-Z_]+$""")) Success()
-    else Failure(s"- ${Messages("cli.param.error.project_key", projectKey)}")
+    if (projectKey.matches("""^[0-9A-Z_]+$""")) ConfigValidateSuccess
+    else ConfigValidateFailure(s"- ${Messages("cli.param.error.project_key", projectKey)}")
 
-  def validateConfigJira(): ConfigValidateResult = {
+  def validateConfigJira(jiraRestClient: JiraRestClient): ConfigValidateResult = {
     ConsoleOut.println(Messages("cli.param.check.access", Messages("common.jira")))
 
     jiraRestClient.myself() match {
       case Right(_) => {
         ConsoleOut.println(Messages("cli.param.ok.access", Messages("common.jira")))
-        Success()
+        ConfigValidateSuccess
       }
       case Left(error: HttpError) => {
         logger.error(error.message, jiraRestClient.url)
         error.clientError match {
-          case AuthenticateFailedError => Failure(s"- ${Messages("cli.param.error.auth", Messages("common.jira"))}")
-          case BadRequestError(detail) => Failure(detail) // TODO: Messages
-          case unknown                 => Failure(s"- ${Messages("cli.param.error.client.unknown", unknown.message)}")
+          case AuthenticateFailedError => ConfigValidateFailure(s"- ${Messages("cli.param.error.auth", Messages("common.jira"))}")
+          case unknown                 => ConfigValidateFailure(s"- ${Messages("cli.param.error.client.unknown", unknown.message)}")
         }
       }
       case Left(e) => {
         logger.error(e.message, e)
-        Failure(s"- ${Messages("cli.param.error.disable.access", Messages("common.jira"))}") // TODO: Messages
+        ConfigValidateFailure(s"- ${Messages("cli.param.error.disable.access", Messages("common.jira"))}")
       }
     }
-//    try {
-//
-//      None
-//    } catch {
-//      case auth: RedmineAuthenticationException =>
-//        logger.error(auth.getMessage, auth)
-//        Some(s"- ${Messages("cli.param.error.auth", Messages("common.jira"))}")
-//      case noauth: NotAuthorizedException =>
-//        logger.error(noauth.getMessage, noauth)
-//        Some(s"- ${Messages("cli.param.error.auth.not.auth", noauth.getMessage)}")
-//      case transport: RedmineTransportException =>
-//        logger.error(transport.getMessage, transport)
-//        Some(s"- ${Messages("cli.param.error.disable.host", Messages("common.jira"), config.redmineConfig.url)}")
-//      case e: Throwable =>
-//        logger.error(e.getMessage, e)
-//        Some(s"- ${Messages("cli.param.error.disable.access", Messages("common.jira"))}")
-//    }
   }
+}
+
+class AppConfigValidator @Inject()(jiraRestClient: JiraRestClient) extends Logging {
+
+  def validate(config: AppConfiguration): List[ConfigValidateResult] = {
+    List(
+      AppConfigValidator.validateProjectKey(config.backlogProjectKey),
+      AppConfigValidator.validateConfigJira(jiraRestClient)
+    ).filter {
+      case ConfigValidateSuccess    => false
+      case ConfigValidateFailure(_) => true
+    }
+  }
+
 }
--- src/main/scala/com/nulabinc/backlog/j2b/conf/ConfigValidateResult.scala
+++ src/main/scala/com/nulabinc/backlog/j2b/conf/ConfigValidateResult.scala
@@ -1,7 +1,3 @@
 package com.nulabinc.backlog.j2b.conf
 
-sealed trait ConfigValidateResult
 
-case class Success() extends ConfigValidateResult
-
-case class Failure(reason: String) extends ConfigValidateResult
