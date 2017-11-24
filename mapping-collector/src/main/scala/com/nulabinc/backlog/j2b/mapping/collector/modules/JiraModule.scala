7dba988290993d28aabaadd7e5ed8cdd71596946
Shoma Nishitateno <shoma.nishitateno@nulab-inc.com>
2017-11-24 20:26:52 +0900

    Enable mapping collector

--- mapping-collector/src/main/scala/com/nulabinc/backlog/j2b/mapping/collector/modules/JiraModule.scala
+++
@@ -1,8 +0,0 @@
-package com.nulabinc.backlog.j2b.mapping.collector.modules
-
-import com.nulabinc.backlog.j2b.jira.conf.JiraApiConfiguration
-import com.nulabinc.backlog.j2b.modules.DefaultModule
-
-private [collector] class JiraModule(apiConfig: JiraApiConfiguration)
-  extends DefaultModule(apiConfig) {}
-
