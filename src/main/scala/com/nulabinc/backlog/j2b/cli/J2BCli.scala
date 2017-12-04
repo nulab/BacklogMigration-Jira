b1024f6712db36e770c5a3ce45b227269853f37e
Shoma Nishitateno <shoma.nishitateno@nulab-inc.com>
2017-12-04 14:33:06 +0900

    Refactor config validation process.

--- src/main/scala/com/nulabinc/backlog/j2b/cli/J2BCli.scala
+++ src/main/scala/com/nulabinc/backlog/j2b/cli/J2BCli.scala
@@ -1,9 +1,9 @@
 package com.nulabinc.backlog.j2b.cli
 
-import com.google.inject.Guice
-import com.nulabinc.backlog.j2b.conf.{AppConfigValidator, AppConfiguration}
+import com.google.inject.{Guice, Injector}
+import com.nulabinc.backlog.j2b.conf.{AppConfigValidator, AppConfiguration, ConfigValidateFailure}
 import com.nulabinc.backlog.j2b.exporter.Exporter
-import com.nulabinc.backlog.j2b.jira.conf.JiraBacklogPaths
+import com.nulabinc.backlog.j2b.jira.conf.{JiraApiConfiguration, JiraBacklogPaths}
 import com.nulabinc.backlog.j2b.jira.converter.MappingConverter
 import com.nulabinc.backlog.j2b.jira.domain.mapping.MappingCollectDatabase
 import com.nulabinc.backlog.j2b.jira.service._
@@ -14,10 +14,13 @@ import com.nulabinc.backlog.migration.common.conf.BacklogConfiguration
 import com.nulabinc.backlog.migration.common.convert.Convert
 import com.nulabinc.backlog.migration.common.modules.{ServiceInjector => BacklogInjector}
 import com.nulabinc.backlog.migration.common.service.{ProjectService, SpaceService, PriorityService => BacklogPriorityService, StatusService => BacklogStatusService, UserService => BacklogUserService}
-import com.nulabinc.backlog.migration.common.utils.Logging
+import com.nulabinc.backlog.migration.common.utils.{ConsoleOut, Logging}
 import com.nulabinc.backlog.migration.importer.core.Boot
 import com.nulabinc.jira.client.JiraRestClient
 import com.nulabinc.jira.client.domain.User
+import com.osinka.i18n.Messages
+
+import scala.util.{Failure, Success, Try}
 
 object J2BCli extends BacklogConfiguration
     with Logging
@@ -31,22 +34,18 @@ object J2BCli extends BacklogConfiguration
 
   def export(config: AppConfiguration): Unit = {
 
-    // Check JIRA configuration is correct. Before creating injector.
-    val jiraClient = JiraRestClient(config.jiraConfig.url, config.jiraConfig.username, config.jiraConfig.password)
-    AppConfigValidator.validateConfigJira(jiraClient)
+    val backlogInjector = BacklogInjector.createInjector(config.backlogConfig)
 
-    startExportMessage()
+    for {
+      _            <- checkJiraApiAccessible(config.jiraConfig)
+      jiraInjector <- createJiraInjector(config)
+      _            <- validateConfig(config, jiraInjector.getInstance(classOf[JiraRestClient]), backlogInjector.getInstance(classOf[SpaceService]))
+    } yield {
+      startExportMessage()
 
-    val jiraInjector    = Guice.createInjector(new ExportModule(config))
-    val jiraRestClient  = jiraInjector.getInstance(classOf[JiraRestClient])
-
-    val backlogInjector         = BacklogInjector.createInjector(config.backlogConfig)
-    val backlogSpaceService     = backlogInjector.getInstance(classOf[SpaceService])
-    val backlogUserService      = backlogInjector.getInstance(classOf[BacklogUserService])
-    val backlogPriorityService  = backlogInjector.getInstance(classOf[BacklogPriorityService])
-    val backlogStatusService    = backlogInjector.getInstance(classOf[BacklogStatusService])
-
-    if (validateConfig(config, jiraRestClient, backlogSpaceService)) {
+      val backlogUserService      = backlogInjector.getInstance(classOf[BacklogUserService])
+      val backlogPriorityService  = backlogInjector.getInstance(classOf[BacklogPriorityService])
+      val backlogStatusService    = backlogInjector.getInstance(classOf[BacklogStatusService])
 
       // Delete old exports
       val jiraBacklogPaths = new JiraBacklogPaths(config.jiraConfig.projectKey, config.backlogConfig.projectKey)
@@ -75,24 +74,22 @@ object J2BCli extends BacklogConfiguration
 
       finishExportMessage()
     }
+
   }
 
   def `import`(config: AppConfiguration): Unit = {
 
-
-    val jiraClient = JiraRestClient(config.jiraConfig.url, config.jiraConfig.username, config.jiraConfig.password)
-    AppConfigValidator.validateConfigJira(jiraClient)
-
-    val jiraInjector    = Guice.createInjector(new ImportModule(config))
     val backlogInjector = BacklogInjector.createInjector(config.backlogConfig)
+    val spaceService    = backlogInjector.getInstance(classOf[SpaceService])
 
-    val jiraRestClient          = jiraInjector.getInstance(classOf[JiraRestClient])
-    val spaceService            = backlogInjector.getInstance(classOf[SpaceService])
-    val backlogUserService      = backlogInjector.getInstance(classOf[BacklogUserService])
-    val backlogPriorityService  = backlogInjector.getInstance(classOf[BacklogPriorityService])
-    val backlogStatusService    = backlogInjector.getInstance(classOf[BacklogStatusService])
-
-    if (validateConfig(config, jiraRestClient, spaceService)) {
+    for {
+      _            <- checkJiraApiAccessible(config.jiraConfig)
+      jiraInjector <- createJiraInjector(config)
+      _            <- validateConfig(config, jiraInjector.getInstance(classOf[JiraRestClient]), spaceService)
+    } yield {
+      val backlogUserService      = backlogInjector.getInstance(classOf[BacklogUserService])
+      val backlogPriorityService  = backlogInjector.getInstance(classOf[BacklogPriorityService])
+      val backlogStatusService    = backlogInjector.getInstance(classOf[BacklogStatusService])
 
       // Mapping file
       val jiraBacklogPaths    = new JiraBacklogPaths(config.jiraConfig.projectKey, config.backlogConfig.projectKey)
@@ -144,5 +141,25 @@ object J2BCli extends BacklogConfiguration
       }
     }
   }
+
+  private def checkJiraApiAccessible(config: JiraApiConfiguration): Option[Unit] = {
+    // Check JIRA configuration is correct. Before creating injector.
+    val jiraClient = JiraRestClient(config.url, config.username, config.password)
+    AppConfigValidator.validateConfigJira(jiraClient) match {
+      case ConfigValidateFailure(failure) =>
+        ConsoleOut.println(Messages("cli.param.error.disable.access", Messages("common.jira")))
+        logger.error(failure)
+        None
+      case _ => Some(())
+    }
+  }
+
+  private def createJiraInjector(config: AppConfiguration): Option[Injector] =
+    Try(Guice.createInjector(new ExportModule(config))) match {
+      case Success(jiraInjector) => Some(jiraInjector)
+      case Failure(error)        =>
+        logger.error(error.getMessage)
+        None
+    }
 
 }
