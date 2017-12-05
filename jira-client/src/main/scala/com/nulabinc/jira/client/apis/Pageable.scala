9998e7f7c2400265a46421e43fe607a487b77db3
Shoma Nishitateno <shoma.nishitateno@nulab-inc.com>
2017-12-05 16:45:51 +0900

    Make comment API pageable

---
+++ jira-client/src/main/scala/com/nulabinc/jira/client/apis/Pageable.scala
@@ -0,0 +1,13 @@
+package com.nulabinc.jira.client.apis
+
+import com.netaporter.uri.Uri
+import com.netaporter.uri.dsl._
+
+// TODO: Define pagination
+trait Pageable {
+
+  def paginateUri(startAt: Long, maxResults: Long): Uri =
+    ("startAt"    -> startAt) &
+    ("maxResults" -> maxResults)
+
+}
