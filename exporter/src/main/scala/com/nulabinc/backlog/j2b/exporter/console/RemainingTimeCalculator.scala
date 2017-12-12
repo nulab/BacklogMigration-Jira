582c11d2ca7e8ab3a7edf7a1e4359bf921fa302b
Shoma Nishitateno <shoma.nishitateno@nulab-inc.com>
2017-12-12 13:21:30 +0900

    Modify display format

--- exporter/src/main/scala/com/nulabinc/backlog/j2b/exporter/console/RemainingTimeCalculator.scala
+++ exporter/src/main/scala/com/nulabinc/backlog/j2b/exporter/console/RemainingTimeCalculator.scala
@@ -5,13 +5,12 @@ import java.util.Date
 import com.nulabinc.backlog.migration.common.utils.{ConsoleOut, DateUtil, Logging, ProgressBar}
 import com.osinka.i18n.Messages
 import org.fusesource.jansi.Ansi
-import org.fusesource.jansi.Ansi.Color._
 import org.fusesource.jansi.Ansi.ansi
 
 class RemainingTimeCalculator(totalSize: Long) extends Logging {
 
-  var failed    = 0
-  var date      = ""
+  private var failed    = 0
+  private var date      = ""
 
   private case class RemainingTime(totalSize: Long, lastTime: Long = System.currentTimeMillis(), totalElapsedTime: Long = 0, count: Long = 0) {
 
@@ -36,39 +35,16 @@ class RemainingTimeCalculator(totalSize: Long) extends Logging {
   private[this] var newLine       = false
   private[this] var isMessageMode = false
 
-  def warning(indexOfDate: Int, totalOfDate: Int, value: String) = {
-    message(indexOfDate: Int, totalOfDate: Int, value: String, YELLOW)
-  }
-
-  def error(indexOfDate: Int, totalOfDate: Int, value: String) = {
-    message(indexOfDate: Int, totalOfDate: Int, value: String, RED)
-  }
-
-  private[this] def message(indexOfDate: Int, totalOfDate: Int, value: String, color: Ansi.Color): Unit = {
-    clear()
-    val message =
-      s"""${(" " * 11) + ansi().fg(color).a(value.replaceAll("\n", "")).reset().toString}
-         |${current(indexOfDate, totalOfDate, value)}
-         |--------------------------------------------------
-         |${remaining()}""".stripMargin
-
-    ConsoleOut.outStream.println(message)
-    isMessageMode = true
-  }
-
-  def progress(indexOfDate: Int, totalOfDate: Int, summary: String) = {
-    newLine = (indexOfDate == 1)
+  def progress(indexOfDate: Int, totalOfDate: Int, summary: String): Unit = {
+    newLine = indexOfDate == 1
     clear()
     remainingTime = remainingTime.action()
-    val message =
-      s"""${current(indexOfDate, totalOfDate, summary)}
-         |--------------------------------------------------
-         |${remaining()}""".stripMargin
+    val message = remaining()
     ConsoleOut.outStream.println(message)
     isMessageMode = false
   }
 
-  private[this] def clear() = {
+  private def clear(): Unit = {
     if (newLine && !isMessageMode) {
       ConsoleOut.outStream.println()
     }
@@ -79,28 +55,9 @@ class RemainingTimeCalculator(totalSize: Long) extends Logging {
     newLine = false
   }
 
-  private[this] def current(indexOfDate: Int, totalOfDate: Int, summary: String): String = {
-    val progressBar  = ProgressBar.progressBar(indexOfDate, totalOfDate)
-    val resultString = if (failed == 0) Messages("common.result_success") else Messages("common.result_failed", failed)
-    val result = if (resultString.nonEmpty) {
-      if (resultString == Messages("common.result_success"))
-        s"[${ansi().fg(GREEN).a(resultString).reset()}]"
-      else s"[${ansi().fg(RED).a(resultString).reset()}]"
-    } else resultString
-
-    val message =
-      Messages("export.date.execute",
-        summary,
-        Messages("common.issues"),
-        if (indexOfDate == totalOfDate) Messages("message.exported") else Messages("message.exporting"))
-
-    s"$progressBar$result $message"
-  }
-
-  private[this] def remaining(): String = {
+  private def remaining(): String = {
     val progressBar = ProgressBar.progressBar(remainingTime.count.toInt, remainingTime.totalSize.toInt)
-    val message     = Messages("export.progress", remainingTime.count, remainingTime.totalSize)
     val time        = Messages("export.remaining_time", DateUtil.timeFormat(new Date(remainingTime.remainingTime)))
-    s"$progressBar $message$time"
+    s"$progressBar $time"
   }
 }
