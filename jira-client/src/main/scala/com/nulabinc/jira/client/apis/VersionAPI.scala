388a13a866b93dc27b5ad55e8ebed446df25fcab
Shoma Nishitateno <shoma.nishitateno@nulab-inc.com>
2018-01-18 11:45:13 +0900

    Supports old version

--- jira-client/src/main/scala/com/nulabinc/jira/client/apis/VersionAPI.scala
+++ jira-client/src/main/scala/com/nulabinc/jira/client/apis/VersionAPI.scala
@@ -1,6 +1,7 @@
 package com.nulabinc.jira.client.apis
 
 import com.nulabinc.jira.client._
+import com.nulabinc.jira.client.domain.Version
 import spray.json._
 
 class VersionAPI(httpClient: HttpClient) {
@@ -12,8 +13,8 @@ class VersionAPI(httpClient: HttpClient) {
   def projectVersions(projectKey: String) = fetch(projectKey)
 
   private def fetch(projectIdOrKey: String) =
-    httpClient.get(s"/project/$projectIdOrKey/version") match {
-      case Right(json)               => Right(JsonParser(json).convertTo[VersionResult].values)
+    httpClient.get(s"/project/$projectIdOrKey/versions") match {
+      case Right(json)               => Right(JsonParser(json).convertTo[Seq[Version]])
       case Left(_: ApiNotFoundError) => Left(ResourceNotFoundError("Version", projectIdOrKey))
       case Left(error)               => Left(HttpError(error))
     }
