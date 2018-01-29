9deb7756065ccf91fc1c27582289bf7645cdf019
Shoma Nishitateno <shoma416@gmail.com>
2018-01-29 18:07:58 +0900

    Refactor: Field definition

--- jira-client/src/main/scala/com/nulabinc/jira/client/domain/field/Field.scala
+++ jira-client/src/main/scala/com/nulabinc/jira/client/domain/field/Field.scala
@@ -3,5 +3,61 @@ package com.nulabinc.jira.client.domain.field
 case class Field(
   id: String,
   name: String,
-  schema: Option[FieldSchema]
+  schema: FieldType
+)
+
+object FieldValueType {
+  case object Single extends FieldValueType
+  case object Multi extends FieldValueType
+}
+
+sealed trait FieldValueType
+
+object FieldType {
+
+  // Primitive types
+  case object String extends FieldType("string")
+  case object Number extends FieldType("number")
+  case object DateTime extends FieldType("datetime")
+  case object Date extends FieldType("date")
+
+  // JIRA specific types
+  case object IssueType extends FieldType("issuetype")
+  case object Project extends FieldType("project")
+  case object FixVersions extends FieldType("fixVersions", FieldValueType.Multi)
+  case object Resolution extends FieldType("resolution")
+  case object Watches extends FieldType("watches")
+  case object Priority extends FieldType("priority")
+  case object Labels extends FieldType("labels", FieldValueType.Multi)
+  case object Versions extends FieldType("versions", FieldValueType.Multi)
+  case object IssueLinks extends FieldType("issuelinks", FieldValueType.Multi)
+  case object User extends FieldType("user")
+  case object Status extends FieldType("status")
+  case object Components extends FieldType("components", FieldValueType.Multi)
+  case object TimeTracking extends FieldType("timetracking")
+  case object Attachments extends FieldType("attachment", FieldValueType.Multi)
+  case object WorkLogs extends FieldType("worklog", FieldValueType.Multi)
+
+  def fromString(value: String): FieldType = value match {
+    case String.value => String
+    case Number.value => Number
+    case DateTime.value => DateTime
+    case Date.value => Date
+    case IssueType.value => IssueType
+    case Project.value => Project
+    case FixVersions.value => FixVersions
+    case Resolution.value => Resolution
+  }
+
+  def apply(schemaType: Option[String], schemaSystem: Option[String], schemaItems: Option[String]): FieldType =
+    (schemaType, schemaSystem, schemaItems) match {
+      case (Some("array"), Some(name), _) => fromString(name)
+      case (Some(name), _, _)             => fromString(name)
+      case (None, _, _)                   => String
+    }
+}
+
+sealed abstract class FieldType(
+  val value: String,
+  fieldValueType: FieldValueType = FieldValueType.Single
 )
--- jira-client/src/main/scala/com/nulabinc/jira/client/json/FieldMappingJsonProtocol.scala
+++ jira-client/src/main/scala/com/nulabinc/jira/client/json/FieldMappingJsonProtocol.scala
@@ -5,37 +5,59 @@ import spray.json._
 
 object FieldMappingJsonProtocol extends DefaultJsonProtocol {
 
-  implicit object FieldSchemaMappingFormat extends RootJsonFormat[FieldSchema] {
-    def write(obj: FieldSchema) = ???
+  implicit object FieldSchemaMappingFormat extends RootJsonFormat[FieldType] {
+    def write(obj: FieldType) = ???
 
-    def read(json: JsValue) = {
+    def read(json: JsValue): FieldType = {
       val jsObject = json.asJsObject
 
       val schemaType = jsObject.getFields("type") match {
-        case Seq(JsString(typeName)) => FieldSchemaType.convert(typeName)
-        case other => deserializationError("Cannot deserialize FieldSchema: invalid input missing `type`. Raw input: " + other)
+        case Seq(JsString(name)) => Some(name)
+        case _                       => None
       }
 
-      val customId = jsObject.getFields("customId") match {
-        case Seq(JsString(id)) => Some(id.toLong)
-        case _ => None
+      val schemaSystem = jsObject.getFields("system") match {
+        case Seq(JsString(name)) => Some(name)
+        case _                       => None
       }
 
-      val customType = jsObject.getFields("custom") match {
-        case Seq(JsString(id)) => FieldCustomType.convert(id)
-        case _ => None
+      val schemaItems = jsObject.getFields("items") match {
+        case Seq(JsString(name)) => Some(name)
+        case _                       => None
       }
 
-      FieldSchema(
-        customId = customId,
-        customType = customType,
-        schemaType = schemaType
-      )
+//      val customId = jsObject.getFields("customId") match {
+//        case Seq(JsString(id)) => Some(id.toLong)
+//        case _ => None
+//      }
+//
+//      val customType = jsObject.getFields("custom") match {
+//        case Seq(JsString(id)) => FieldCustomType.convert(id)
+//        case _ => None
+//      }
+
+      FieldType(schemaType = schemaType, schemaSystem = schemaSystem, schemaItems = schemaItems)
     }
 
 
   }
 
-  implicit val fieldMappingFormat = jsonFormat3(Field)
+  implicit object fieldMappingFormat extends RootJsonFormat[Field] {
+    override def write(obj: Field): JsValue = ???
+
+    override def read(json: JsValue): Field = {
+      val jsObject = json.asJsObject
+
+      jsObject.getFields("id", "name", "schema") match {
+        case Seq(JsString(id), JsString(name), schema) => Field(
+          id = id,
+          name = name,
+          schema = schema.convertTo[FieldType]
+        )
+        case _ => deserializationError(s"Not supported schema")
+      }
+    }
+
+  }
 
 }
