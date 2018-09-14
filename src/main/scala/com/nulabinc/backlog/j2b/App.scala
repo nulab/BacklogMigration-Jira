ae10d191546e7673316f8954575ca6b93bce375a
Shoma Nishitateno <shoma416@gmail.com>
2018-09-14 15:21:30 +0900

    wip: app program

--- src/main/scala/com/nulabinc/backlog/j2b/App.scala
+++ src/main/scala/com/nulabinc/backlog/j2b/App.scala
@@ -2,7 +2,7 @@ package com.nulabinc.backlog.j2b
 
 import com.nulabinc.backlog.j2b.cli.J2BCli
 import com.nulabinc.backlog.j2b.conf.AppConfiguration
-import com.nulabinc.backlog.j2b.core.{CommandLineInterface, GithubRelease, NextCommand}
+import com.nulabinc.backlog.j2b.core.{CommandLineInterface, GithubRelease}
 import com.nulabinc.backlog.j2b.dsl.{AppDSL, ConsoleDSL}
 import com.nulabinc.backlog.j2b.interpreters.{AsyncAppInterpreter, AsyncConsoleInterpreter}
 import com.nulabinc.backlog.j2b.jira.conf.JiraApiConfiguration
@@ -11,8 +11,8 @@ import com.nulabinc.backlog.migration.common.conf.{BacklogApiConfiguration, Back
 import com.nulabinc.backlog.migration.common.utils.{ConsoleOut, Logging}
 import com.osinka.i18n.Messages
 import monix.eval.Task
-import org.fusesource.jansi.AnsiConsole
 import monix.execution.Scheduler
+import org.fusesource.jansi.AnsiConsole
 
 import scala.util.{Failure, Success, Try}
 
@@ -22,6 +22,8 @@ object App extends BacklogConfiguration with Logging {
   def main(args: Array[String]): Unit = {
 
     // DisableSSLCertificateCheckUtil.disableChecks() // TODO: ???
+
+    AnsiConsole.systemInstall()
 
     if ( ! ClassVersion.isValid()) {
       ConsoleOut.error(Messages("cli.require_java8", System.getProperty("java.specification.version")))
@@ -35,16 +37,26 @@ object App extends BacklogConfiguration with Logging {
     )
 
     val program = for {
-      _ <- AppDSL.pure(AnsiConsole.systemInstall()) // Console initialization
       _ <- AppDSL.fromConsole(ConsoleDSL.print(startMessage(applicationName)))
+//      _ <- AppDSL.pure(GithubRelease.checkRelease()) // TODO:
       _ <- AppDSL.setLanguage(language)
-      _ <- AppDSL.pure(GithubRelease.checkRelease()) // TODO:
+      _ <- getConfiguration(new CommandLineInterface(args)) match {
+        case Success(config) =>
+          for {
+            _ <- AppDSL.fromConsole(ConsoleDSL.print(configurationMessage(config)))
+          } yield ()
+        case Failure(error) =>
+          logger.error(error.getMessage)
+          J2BCli.help()
+          for {
+            _ <- AppDSL.fromConsole(ConsoleDSL.error(s"${Messages("cli.error.args")}"))
+            _ <- AppDSL.exit(1)
+          } yield ()
+      }
     } yield ()
 
     val cleanup = interpreter.terminate().flatMap(_ =>
-      Task {
-        AnsiConsole.systemUninstall()
-      }
+      Task.unit
     )
 
     interpreter
@@ -56,27 +68,21 @@ object App extends BacklogConfiguration with Logging {
           exit(1, ex)
         }
       }
-      .foreach { _ =>
-        // Run
-        val cli = new CommandLineInterface(args)
-
-        getConfiguration(cli) match {
-          case Success(config) =>
-            ConsoleOut.println(configurationMessage(config))
-            cli.subcommand match {
-              case Some(cli.importCommand) => J2BCli.`import`(config)
-              case Some(cli.exportCommand) => J2BCli.export(config, NextCommand.command(args))
-              case _                       => J2BCli.help()
-            }
-            exit(0)
-          case Failure(failure) =>
-            ConsoleOut.error(s"${Messages("cli.error.args")}")
-            logger.error(failure.getMessage)
-            J2BCli.help()
-            exit(1)
-        }
-      }
-
+      .runAsync
+//      .foreach { _ =>
+//        // Run
+//
+//
+//        getConfiguration(cli) match {
+//          case Success(config) =>
+//            cli.subcommand match {
+//              case Some(cli.importCommand) => J2BCli.`import`(config)
+//              case Some(cli.exportCommand) => J2BCli.export(config, NextCommand.command(args))
+//              case _                       => J2BCli.help()
+//            }
+//            exit(0)
+//        }
+//      }
   }
 
   private[this] def getConfiguration(cli: CommandLineInterface) = Try {
