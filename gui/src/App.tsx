d3042ef78a17a711e807ef8961e0f198af728902
Shoma Nishitateno <shoma416@gmail.com>
2019-05-28 20:03:33 +0900

    add: style

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -16,7 +16,7 @@ const App: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React  0000000000000000
+          Learn React
         </a>
       </header>
     </div>
