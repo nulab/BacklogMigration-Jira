9b9be8ad7c294621776477275536c9d26516bf5f
Shoma Nishitateno <shoma416@gmail.com>
2019-06-05 14:22:07 +0900

    clean: lint for single quote

--- gui/src/App.tsx
+++ gui/src/App.tsx
@@ -4,25 +4,25 @@ import { Label } from './components/atoms/Label';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <form id="myForm" onSubmit={() => {}}>
+    <div className='App'>
+      <form id='myForm' onSubmit={() => {}}>
         <fieldset>
-          <div className="row">
-            <Label htmlFor="spaceID">Space ID</Label>
-            <div className="space-id-field">
+          <div className='row'>
+            <Label htmlFor='spaceID'>Space ID</Label>
+            <div className='space-id-field'>
               <input
-                id="spaceID"
-                type="text"
-                name="space"
-                value="<?= config.space ?>"
+                id='spaceID'
+                type='text'
+                name='space'
+                value='<?= config.space ?>'
                 tabIndex={1}
-                className="space-id-field__id"
+                className='space-id-field__id'
               />
-              <span className="space-id-field__name">.backlog</span>
+              <span className='space-id-field__name'>.backlog</span>
               <select
-                name="domain"
+                name='domain'
                 tabIndex={2}
-                className="space-id-field__domain"
+                className='space-id-field__domain'
               >
                 {/* <? if (config.domain == ".com") { ?>
                  <option value=".com" selected>.com</option>
@@ -37,34 +37,34 @@ const App: React.FC = () => {
               </select>
             </div>
           </div>
-          <div className="row">
-            <label htmlFor="apiKey">
+          <div className='row'>
+            <label htmlFor='apiKey'>
               {/* <?= getMessage("label_apiKey") ?> */}
             </label>
             <input
-              id="apiKey"
-              type="text"
-              name="apiKey"
-              value="<?= config.apiKey ?>"
+              id='apiKey'
+              type='text'
+              name='apiKey'
+              value='<?= config.apiKey ?>'
               tabIndex={3}
             />
           </div>
-          <div className="row">
-            <label htmlFor="projectKey">
+          <div className='row'>
+            <label htmlFor='projectKey'>
               {/* <?= getMessage("label_projectKey") ?> */}
             </label>
             <input
-              id="projectKey"
-              type="text"
-              name="projectKey"
-              value="<?= config.projectKey ?>"
+              id='projectKey'
+              type='text'
+              name='projectKey'
+              value='<?= config.projectKey ?>'
               tabIndex={4}
             />
           </div>
-          <div className="button-area">
+          <div className='button-area'>
             <input
-              type="submit"
-              className="button"
+              type='submit'
+              className='button'
               value="<?= getMessage('button_execute') ?>"
               tabIndex={5}
             />
