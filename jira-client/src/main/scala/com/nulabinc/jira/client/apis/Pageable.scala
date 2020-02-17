93bbd4fdac3e98cc6350a2bf6c2efcfedd868d74
Shoma Nishitateno <shoma.nishitateno@nulab.com>
2020-02-17 14:22:50 +0900

    BLGMIGRATION-865 fix: type

--- jira-client/src/main/scala/com/nulabinc/jira/client/apis/Pageable.scala
+++ jira-client/src/main/scala/com/nulabinc/jira/client/apis/Pageable.scala
@@ -1,11 +1,11 @@
 package com.nulabinc.jira.client.apis
 
-import io.lemonlabs.uri.Uri
+import io.lemonlabs.uri.Url
 import io.lemonlabs.uri.dsl._
 
 trait Pageable {
 
-  def paginateUri(startAt: Long, maxResults: Long): Uri =
+  def paginateUri(startAt: Long, maxResults: Long): Url =
     ("startAt"    -> startAt) &
     ("maxResults" -> maxResults)
 
