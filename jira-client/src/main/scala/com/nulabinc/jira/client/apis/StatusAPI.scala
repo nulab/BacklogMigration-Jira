c7ed6789d3ad8eade8a052fe8d0273810ad993af
Shoma Nishitateno <shoma.nishitateno@nulab.com>
2020-03-19 17:09:28 +0900

    BLGMIGRATION-866 fix: status api response is wrong

--- jira-client/src/main/scala/com/nulabinc/jira/client/apis/StatusAPI.scala
+++ jira-client/src/main/scala/com/nulabinc/jira/client/apis/StatusAPI.scala
@@ -10,9 +10,15 @@ class StatusAPI(httpClient: HttpClient) {
 
   def statuses(projectIdOrKey: String): Either[JiraRestClientError, Seq[Status]] = {
     httpClient.get(s"/project/$projectIdOrKey/statuses") match {
-      case Right(json)               => Right(JsonParser(json).convertTo[Seq[Status]])
-      case Left(_: ApiNotFoundError) => Left(ResourceNotFoundError("status", ""))
-      case Left(error)               => Left(HttpError(error))
+      case Right(json) =>
+        val statuses = JsonParser(json).convertTo[Seq[StatusWrapper]].foldLeft(Set.empty[Status]) { (acc, item) =>
+          acc ++ item.statuses.toSet
+        }.toSeq
+        Right(statuses)
+      case Left(_: ApiNotFoundError) =>
+        Left(ResourceNotFoundError("status", ""))
+      case Left(error) =>
+        Left(HttpError(error))
     }
   }
 }
