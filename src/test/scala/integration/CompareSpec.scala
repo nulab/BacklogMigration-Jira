1fe705105ad79c09a137a6267e916169e0235eab
Shoma Nishitateno <shoma.nishitateno@nulab.com>
2020-04-07 11:11:56 +0900

    BLGMIGRATION-866 wip: integration test

--- src/test/scala/integration/CompareSpec.scala
+++ src/test/scala/integration/CompareSpec.scala
@@ -1,17 +1,25 @@
 package integration
 
+import java.io.File
+
 import com.nulabinc.backlog.j2b.jira.conf.JiraApiConfiguration
 import com.nulabinc.backlog.j2b.jira.domain.export._
+import com.nulabinc.backlog.j2b.jira.domain.mapping.JiraUserMappingItem
 import com.nulabinc.backlog.j2b.jira.domain.{FieldConverter, IssueFieldConverter}
+import com.nulabinc.backlog.j2b.mapping.core.MappingDirectory
 import com.nulabinc.backlog.migration.common.conf.{BacklogApiConfiguration, BacklogConstantValue}
-import com.nulabinc.backlog.migration.common.convert.Convert
 import com.nulabinc.backlog.migration.common.convert.writes.UserWrites
+import com.nulabinc.backlog.migration.common.dsl.{AppDSL, ConsoleDSL, StorageDSL}
+import com.nulabinc.backlog.migration.common.interpreters.{JansiConsoleDSL, LocalStorageDSL, TaskAppDSL}
+import com.nulabinc.backlog.migration.common.services.UserMappingFileService
 import com.nulabinc.backlog4j.api.option.{GetIssuesParams, QueryParams}
 import com.nulabinc.backlog4j.internal.json.customFields._
 import com.nulabinc.backlog4j.{CustomFieldSetting, IssueComment}
 import com.nulabinc.jira.client.domain.changeLog.LinkChangeLogItemField
 import integration.helper.{DateFormatter, TestHelper}
 import integration.matchers.{DateMatcher, UserMatcher}
+import monix.eval.Task
+import monix.execution.Scheduler.Implicits.global
 import org.scalatest.{DiagrammedAssertions, FlatSpec, Matchers}
 
 import scala.collection.JavaConverters._
@@ -24,6 +32,12 @@ class CompareSpec extends FlatSpec
     with DateFormatter
     with UserMatcher
     with DateMatcher {
+
+  import com.nulabinc.backlog.j2b.deserializers.JiraMappingDeserializer._
+
+  private implicit val appDSL: AppDSL[Task] = TaskAppDSL()
+  private implicit val storageDSL: StorageDSL[Task] = LocalStorageDSL()
+  private implicit val consoleDSL: ConsoleDSL[Task] = JansiConsoleDSL()
 
   val jiraCustomFieldDefinitions: Seq[Field] = FieldConverter.toExportField(jiraRestApi.fieldAPI.all().right.get)
   val backlogCustomFieldDefinitions: mutable.Seq[CustomFieldSetting] = backlogApi.getCustomFields(appConfig.backlogConfig.projectKey).asScala
@@ -53,14 +67,22 @@ class CompareSpec extends FlatSpec
     "Project user" should "match" in {
       implicit val backlogUserWrites: UserWrites = new UserWrites()
 
-      val backlogUsers    = backlogApi.getProjectUsers(backlogConfig.projectKey).asScala
-      val jiraUsers       = userMappingFile.tryUnMarshal()
+      val backlogUsers = backlogApi.getProjectUsers(backlogConfig.projectKey).asScala
+      val jiraUsersResult = UserMappingFileService
+        .getMappings[JiraUserMappingItem, Task](new File(MappingDirectory.USER_MAPPING_FILE).getAbsoluteFile.toPath)
+        .runSyncUnsafe()
 
-      jiraUsers.foreach { jiraUser =>
-        backlogUsers.exists { backlogUser =>
-          backlogUser.getUserId == jiraUser.dst
-        } should be(true)
+      jiraUsersResult match {
+        case Right(jiraUsers) =>
+          jiraUsers.foreach { jiraUser =>
+            backlogUsers.exists { backlogUser =>
+              backlogUser.getUserId == jiraUser.optDst.map(_.value).getOrElse(fail("dst item is not defined"))
+            } should be(true)
+          }
+        case Left(error) =>
+          fail(error.toString)
       }
+
     }
   }
 
