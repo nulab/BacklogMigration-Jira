b22a6e4a419085f3706cf9581c3d8c6f716097b7
Shoma Nishitateno <shoma.nishitateno@nulab.com>
2020-12-02 10:05:55 +0900

    BLGMIGRATION-923 update: common to 0.3.2

--- src/main/scala/com/nulabinc/backlog/j2b/mapping/converter/MappingUserConverter.scala
+++ src/main/scala/com/nulabinc/backlog/j2b/mapping/converter/MappingUserConverter.scala
@@ -4,10 +4,7 @@ import com.nulabinc.backlog.j2b.jira.domain.mapping.ValidatedJiraUserMapping
 import com.nulabinc.backlog.j2b.mapping.converter.writes.UserWrites
 import com.nulabinc.backlog.migration.common.convert.Convert
 import com.nulabinc.backlog.migration.common.domain.BacklogUser
-import com.nulabinc.backlog.migration.common.domain.mappings.{
-  BacklogUserMappingItem,
-  IdUserMappingType
-}
+import com.nulabinc.backlog.migration.common.domain.mappings.BacklogUserMappingItem
 import com.nulabinc.backlog.migration.common.utils.{ConsoleOut, Logging}
 import com.osinka.i18n.Messages
 
@@ -38,11 +35,7 @@ object MappingUserConverter extends Logging {
   ): ValidatedJiraUserMapping =
     mappings.find(_.dst.value == accountId.trim) match {
       case Some(mapping) =>
-        ValidatedJiraUserMapping(
-          mapping.src,
-          BacklogUserMappingItem(accountId),
-          IdUserMappingType
-        )
+        ValidatedJiraUserMapping(mapping.src, BacklogUserMappingItem(accountId))
       case _ =>
         mappings.find(_.src.accountId == accountId.trim) match {
           case Some(mapping) =>
