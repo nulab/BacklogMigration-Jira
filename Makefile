0ca6a439819734d6e698019f05492ed78e0d76c0
Shoma Nishitateno <shoma.nishitateno@nulab.com>
2021-01-23 00:59:17 +0900

    BLGMIGRATION-948 feat: define jira status table

--- Makefile
+++ Makefile
@@ -3,6 +3,9 @@
 build:  ## Build a jar file
 	@sbt clean "testOnly com.nulabinc.*" assembly
 
+testwatch: ## Unit test
+	sbt "~testOnly com.nulabinc.*"
+
 .PHONY: help
 help:
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
---
+++ src/test/scala/com/nulabinc/jira/persistence/store/JiraSQLiteStoreDSLSpec.scala
@@ -0,0 +1,37 @@
+package com.nulabinc.jira.persistence.store
+
+import com.nulabinc.backlog.j2b.persistence.store.JiraSQLiteStoreDSL
+import com.nulabinc.jira.client.domain.Status
+import monix.execution.Scheduler
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.must.Matchers
+
+import java.nio.file.Paths
+
+trait TestFixture {
+  implicit val sc: Scheduler = monix.execution.Scheduler.global
+  private val dbPath         = Paths.get("./jira.db")
+
+  val dsl = JiraSQLiteStoreDSL(dbPath)
+
+  def setup(): Unit = {
+    dbPath.toFile.delete()
+    dsl.createTable.runSyncUnsafe()
+  }
+}
+
+class JiraSQLiteStoreDSLSpec extends AnyFunSuite
+  with Matchers
+  with TestFixture {
+  val status1 = Status("123", "name1")
+  val status2 = Status("id2", "name2")
+  val statuses = Seq(status1, status2)
+
+  test("store Jira statuses and find") {
+    setup()
+
+    dsl.storeJiraStatuses(statuses).runSyncUnsafe() mustBe ()
+    dsl.allJiraStatus.runSyncUnsafe() mustBe statuses
+    dsl.findJiraStatus("123").runSyncUnsafe() mustBe Some(status1)
+  }
+}
