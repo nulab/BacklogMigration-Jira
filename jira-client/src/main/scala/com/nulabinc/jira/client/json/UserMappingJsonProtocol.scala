6b9dca8b9ed85d85e507c26f4f6dedd356435aea
Shoma Nishitateno <shoma.nishitateno@nulab.com>
2021-03-04 20:39:15 +0900

    BLGUNYO-75765 fix: support jira server

--- jira-client/src/main/scala/com/nulabinc/jira/client/json/UserMappingJsonProtocol.scala
+++ jira-client/src/main/scala/com/nulabinc/jira/client/json/UserMappingJsonProtocol.scala
@@ -3,6 +3,31 @@ package com.nulabinc.jira.client.json
 import com.nulabinc.jira.client.domain.User
 import spray.json._
 
+import scala.util.Try
+
 object UserMappingJsonProtocol extends DefaultJsonProtocol {
-  implicit val userMappingFormat = jsonFormat3(User)
+
+  // Supports JIRA server
+  implicit object UserFormats extends RootJsonFormat[User] {
+    private val userMappingFormat = jsonFormat3(User)
+
+    def read(json: JsValue) =
+      Try(userMappingFormat.read(json)).getOrElse {
+        val obj = json.asJsObject
+        obj.getFields("key", "name", "emailAddress") match {
+          case Seq(JsString(key), JsString(name), JsString(email)) =>
+            User(
+              accountId = key,
+              displayName = name,
+              emailAddress = Some(email)
+            )
+          case Seq(JsString(key), JsString(name)) =>
+            User(accountId = key, displayName = name, emailAddress = None)
+          case others =>
+            throw deserializationError("Cannot deserialize User. Input: " + others)
+        }
+      }
+    override def write(obj: User): JsValue = ???
+  }
+
 }
